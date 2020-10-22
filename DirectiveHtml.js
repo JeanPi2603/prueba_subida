194c276d0ebfdd30a0ff06c903f4bf22d602068b
Jean Pierre <jean.nique@unmsm.edu.pe>
2020-10-22 01:06:20 -0500

    modifique el js

--- DirectiveHtml.js
+++ DirectiveHtml.js
@@ -1,9 +1,5 @@
 let DirectiveHtml = {
 
-    template: `<div>
-                <h1 v-text="text"></h1>
-                <p v-html="message"></p>
-                </div>`,
     data() {
         return {
             text: 'Directiva v-html',
